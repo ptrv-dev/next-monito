1edb74031bf39a6741c90d235ea1beb8343b8249
ptrv-dev <petrov99119@gmail.com>
2022-10-22 21:34:27 +0300

    Hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Hero from '../components/IndexPage/Hero';
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
         />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
+      <Hero />
     </>
   );
 };
