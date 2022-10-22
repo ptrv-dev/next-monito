8a66a7238f0c7b7d9121ac31722d776c9e7a6cab
ptrv-dev <petrov99119@gmail.com>
2022-10-22 21:07:22 +0300

    Header

---
+++ components/Header/index.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import React from 'react';
+import Search from '../Search';
+
+import style from './Header.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={style.header}>
+      <div className={`${style.headerContainer} container`}>
+        <div className={style.headerColumn}>
+          <Link href={'/'}>
+            <a className={style.headerLogo}>
+              <img src="/static/img/logo.svg" alt="Monito Logo" />
+            </a>
+          </Link>
+          <nav className={style.headerNav}>
+            <ul>
+              <li>
+                <Link href={'/'}>
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={'/'}>
+                  <a>Category</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={'/'}>
+                  <a>About</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={'/'}>
+                  <a>Contact</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={style.headerColumn}>
+          <Search />
+          <button className={`${style.headerButton} button`}>
+            Join the community
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
