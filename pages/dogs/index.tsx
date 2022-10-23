71c64082cb181cbf4caff130abd8a0070827a1c9
ptrv-dev <petrov99119@gmail.com>
2022-10-23 20:20:37 +0300

    Dog Full Page

---
+++ pages/dogs/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+import style from '../../styles/DogsPage.module.scss';
+
+const DogsPage: NextPage = () => {
+  return (
+    <main className={`${style.root}`}>
+      <div className="container">123</div>
+    </main>
+  );
+};
+
+export default DogsPage;
