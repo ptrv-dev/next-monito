035ff973495fac6e0bebfefd0aa5bfac2b76f26b
ptrv-dev <petrov99119@gmail.com>
2022-10-26 14:13:43 +0300

    Fixes

--- components/Subscribe/index.tsx
+++ components/Subscribe/index.tsx
@@ -5,7 +5,7 @@ import style from './Subscribe.module.scss';
 const Subscribe: React.FC = () => {
   return (
     <div className={`${style.subscribe}`}>
-      <h2>Register now so you don't miss our programs</h2>
+      <h2>Register now so you don&apos;t miss our programs</h2>
       <form>
         <input type="text" placeholder="Enter your Email" />
         <button type="submit">Subscribe now</button>
